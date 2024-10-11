2be165fadbc96567fc4d757d4e3c55260a3551b0
Windows 10 <nguyenthanhson.19102003@gmail.com>
2024-10-11 22:25:32 +0700

    BE 20

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,8 +1,10 @@
 const Orders = require('../models/orders');
+const OrderDetails = require('../models/order_details');
 
 class OrderController {
     getList(req, res, next) {
         Orders.find({})
+            .populate('account_id', 'username')
             .then((orders) => {
                 res.status(200).json(orders);
             })
@@ -30,6 +32,7 @@ class OrderController {
     }
     getOrderById(req, res, next) {
         Orders.findById(req.params.orderId)
+            .populate('account_id', 'username')
             .then((order) => {
                 if (!order) return res.status(404).json({ message: 'order not found' });
                 res.status(200).json(order);
@@ -53,6 +56,82 @@ class OrderController {
             .then((order) => {
                 if (!order) return res.status(404).json({ message: 'order not found' });
                 res.status(200).json(order);
+            })
+            .catch((err) => {
+                res.status(500).json({ error: err.message });
+            });
+    }
+    getOrderByAccountId(req, res, next) {
+        Orders.find({ accountId: req.params.accountId })
+            .populate('account_id', 'username')
+            .then((orders) => {
+                if (!orders) return res.status(404).json({ message: 'orders not found' });
+                res.status(200).json(orders);
+            })
+            .catch((err) => {
+                res.status(500).json({ error: err.message });
+            });
+    }
+    getOrderHistoryByAccountId(req, res, next) {
+        Orders.find({ accountId: req.params.accountId, status: { $in: ['shipped', 'cancelled'] } })
+            .then((orders) => {
+                if (!orders) return res.status(404).json({ message: 'orders not found' });
+                res.status(200).json(orders);
+            })
+            .catch((err) => {
+                res.status(500).json({ error: err.message });
+            });
+    }
+    getOrderListByAccountId(req, res, next) {
+        Orders.find({ accountId: req.params.accountId, status: { $in: ['delivered', 'processing', 'pending'] } })
+            .then((orders) => {
+                if (!orders) return res.status(404).json({ message: 'orders not found' });
+                res.status(200).json(orders);
+            })
+            .catch((err) => {
+                res.status(500).json({ error: err.message });
+            });
+    }
+    confirmOrder(req, res, next) {
+        Orders.findByIdAndUpdate(req.params.orderId, { status: 'processing' }, { new: true })
+            .populate('account_id', 'username')
+            .then((order) => {
+                if (!order) return res.status(404).json({ message: 'order not found' });
+                res.status(200).json(order);
+                setTimeout(() => {
+                    Orders.findByIdAndUpdate(req.params.orderId, { status: 'delivered' }, { new: true })
+                        .then((updateOrder) => {
+                            console.log('order has been delivered');
+                        })
+                        .catch((err) => {
+                            console.log('Error updating order status to delivered', err);
+                        });
+                }, 60000)
+            })
+            .catch((err) => {
+                res.status(500).json({ error: err.message });
+            });
+    }
+    cancelOrder(req, res, next) {
+        Orders.findByIdAndUpdate(req.params.orderId, { status: 'cancelled' }, { new: true })
+            .populate('account_id', 'username')
+            .then((order) => {
+                if (!order) return res.status(404).json({ message: 'order not found' });
+                res.status(200).json(order);
+            })
+            .catch((err) => {
+                res.status(500).json({ error: err.message });
+            });
+    }
+    getOrderDetails(req, res, next) {
+        OrderDetails.find({ order: req.params.orderId })
+            .populate('order_id', 'total_price', 'phone', 'address', 'status')
+            .populate('pant_shirt_size_detail_id', 'shirt_id', 'pant_id', 'size_id')
+            .populate('shoes_size_detail_id', 'shoes_id', 'size_id')
+            .populate('accessory_id', 'name', 'quantity', 'price')
+            .then((details) => {
+                if (!details) return res.status(404).json({ message: 'order details not found' });
+                res.status(200).json(details);
             })
             .catch((err) => {
                 res.status(500).json({ error: err.message });
--- src/routes/order.js
+++ src/routes/order.js
@@ -5,9 +5,15 @@ var orderRouter = express.Router();
 
 orderRouter.get('/', orderController.getList);
 orderRouter.post('/', orderController.createOrder);
-orderRouter.delete('/', orderController.deleteOrderById);
+orderRouter.delete('/', orderController.deleteAllOrder);
 orderRouter.get('/:orderId', orderController.getOrderById);
 orderRouter.put('/:orderId', orderController.updateOrderById);
 orderRouter.delete('/:orderId', orderController.deleteOrderById);
+orderRouter.get('/orderId', orderController.getOrderDetails);
+orderRouter.get('/:accountId', orderController.getOrderByAccountId);
+orderRouter.get('/accountId', orderController.getOrderHistoryByAccountId);
+orderRouter.get('/:accountId', orderController.getOrderListByAccountId);
+orderRouter.put('/:accountId', orderController.confirmOrder);
+orderRouter.put('/:accountId', orderController.cancelOrder);
 
 module.exports = orderRouter;
