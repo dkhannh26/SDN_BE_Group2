53efc1eb90fa8aa4045cf81ddfad493199b6afe6
Windows 10 <nguyenthanhson.19102003@gmail.com>
2024-10-11 12:35:49 +0700

    19

--- src/routes/voucher.js
+++ src/routes/voucher.js
@@ -5,8 +5,8 @@ var voucherRouter = express.Router();
 
 voucherRouter.get('/', voucherController.getList);
 voucherRouter.post('/', voucherController.createVoucher);
-voucherRouter.get('/voucherId', voucherController.getVoucherById);
-voucherRouter.put('/voucherId', voucherController.updateVoucherById);
-voucherRouter.delete('/voucherId', voucherController.deleteVoucherById);
+voucherRouter.get('/:voucherId', voucherController.getVoucherById);
+voucherRouter.put('/:voucherId', voucherController.updateVoucherById);
+voucherRouter.delete('/:voucherId', voucherController.deleteVoucherById);
 
 module.exports = voucherRouter;
