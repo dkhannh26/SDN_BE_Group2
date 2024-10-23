e81bfefca2d15fda0459159e2e913db43e0d5885
ThinhLam <thinhlam240499@gmail.com>
2024-10-23 18:54:54 +0700

    ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,13 @@ var express = require("express");
 
 var router = express.Router();
 var accountRouter = require("./accountRouter");
+var adminRouter = require("./adminRouter");
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
 router.use("/account", accountRouter);
+router.use("/admin", adminRouter);
+
 module.exports = router;
