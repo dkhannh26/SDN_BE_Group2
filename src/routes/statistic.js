09bc1c5da02692beb62c849691ac88ecdf96758a
ThinhLam <thinhlam240499@gmail.com>
2024-11-04 23:45:33 +0700

    add export

--- src/routes/statistic.js
+++ src/routes/statistic.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const { getStatistic } = require('../controllers/statistic.controller');
-const statisticRouter = express.Router()
+const express = require("express");
+const {
+  getStatistic,
+  exportExcel,
+} = require("../controllers/statistic.controller");
+const statisticRouter = express.Router();
 
-statisticRouter.get('/:year', getStatistic)
+statisticRouter.post("/export", exportExcel);
+
+statisticRouter.get("/:year", getStatistic);
 
 module.exports = statisticRouter;
